a1f206550e723526a94ff8264fe7f79c4dfd5747
黒河 飛翔 <k023g0112@g.neec.ac.jp>
2023-11-08 12:55:44 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <Novice.h>
 
 const char kWindowTitle[] = "LC1B_12_クロカワツバサ_タイトル";
+const int kWindowSizeX = 1280;
+const int kWindowSizeY = 720;
 
 
 float powf(float base, float exponent) {
@@ -17,6 +19,7 @@ struct Vector2 {
 	float y;
 };
 
+// ボール構造体
 struct Ball {
 	Vector2 pos_;
 	float radius_;
@@ -24,19 +27,39 @@ struct Ball {
 	Vector2 startPos_;
 	Vector2 endPos_;
 
+	float t_;
+	int moveTime_;
+
 	Ball();
 
+	//描画
+	void Draw();
+
+	//更新処理
+	void Update();
 };
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+	Novice::Initialize(kWindowTitle, kWindowSizeX, kWindowSizeY);
 
-	Ball ball;
+	const int kMaxBall = 4;
+
+	Ball ball[kMaxBall];
+
+	for (int i = 0; i < kMaxBall; i++) {
+
+		ball[i].startPos_.y = ((kWindowSizeY / kMaxBall) * float(i)) + ball[i].radius_;
+		ball[i].endPos_.y = ball[i].startPos_.y;
+
+		//初期地点を代入
+		ball[i].pos_ = ball[i].startPos_;
+	}
+
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -50,7 +73,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-		
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -58,6 +81,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		for (int i = 0; i < kMaxBall; i++) {
+		
+			ball[i].Draw();
+
+			Novice::ScreenPrintf(20, 20 + 20 * i, "%f,%f", ball[i].pos_.x, ball[i].pos_.y);
+		}
+		
 
 		///
 		/// ↑描画処理ここまで
@@ -78,4 +109,44 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 }
 
 
-Ball::Ball()
+Ball::Ball() {
+
+	radius_ = 50.0f;
+
+
+	startPos_ = {
+	radius_,
+	0.0f
+	};
+
+	endPos_ = {
+		kWindowSizeX - radius_,
+		0.0f
+	};
+
+	pos_ = startPos_;
+
+	color_ = 0xff0000ff;
+
+
+	moveTime_ = 120;
+}
+
+void Ball::Update() {
+
+	t_ += (1.0f / moveTime_);
+}
+
+//描画
+void Ball::Draw() {
+
+	Novice::DrawEllipse(
+		int(pos_.x),
+		int(pos_.y),
+		int(radius_),
+		int(radius_),
+		0.0f,
+		color_,
+		kFillModeSolid
+	);
+}
